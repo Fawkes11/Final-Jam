26b97aa67cf302dd1a6223c676eae9e283e459fa
DaskalosJSM <116764636+DaskalosJSM@users.noreply.github.com>
2022-12-26 12:19:03 -0500

    fixed target system

--- Assets/Scripts/PlayerCombatMovement.cs
+++ Assets/Scripts/PlayerCombatMovement.cs
@@ -38,8 +38,8 @@ public class PlayerCombatMovement : MonoBehaviour
         {
             int MemoryIndex;
             MemoryIndex = EnemyIndex;
-            if (EnemyIndex >= Sensor.enemyInRange.Count - 1) EnemyIndex = 0;
-            if (EnemyIndex < Sensor.enemyInRange.Count - 1) EnemyIndex += 1;
+            if (EnemyIndex < Sensor.enemyInRange.Count) EnemyIndex += 1;
+            if (EnemyIndex >= Sensor.enemyInRange.Count) EnemyIndex = 0;
             target = Sensor.enemyInRange[MemoryIndex].gameObject;
             target.GetComponent<UIEnemyelements>().DisableOutline();
         }
